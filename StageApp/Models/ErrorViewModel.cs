4bb0063a44cf873485a7831c0cc8c2422802c594
Milanh3h3 <149052315+Milanh3h3@users.noreply.github.com>
2024-12-12 11:44:24 +0100

    Project aangemaakt

---
+++ StageApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StageApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
